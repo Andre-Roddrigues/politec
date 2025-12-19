7c5dcf449362fd334134273ca08c9e7cc148d37f
Andre-Roddrigues <lariotyler6@gmail.com>
2025-12-19 18:58:44 +0200

    feat: meta

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroPolitec from "../components/Landing/Hero2/HeroSection";
 
 export const metadata: Metadata = {
   title: "POLITEC",
-  description: "Guia para preencher seu currículo na Unitec, incluindo informações pessoais, formação acadêmica e experiências relevantes para a graduação.",
+  // description: "Guia para preencher seu currículo na Unitec, incluindo informações pessoais, formação acadêmica e experiências relevantes para a graduação.",
 };
 export default function Home() {
 
